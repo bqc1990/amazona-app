ef2ac556687ee172c9e6248372280a928088bfd1
bqc1990 <bao.jason1990@gmail.com>
2021-03-21 15:59:57 -0400

    share code on github

---
+++ app/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      image: "/images/p1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      reviews: 10,
+      description: "high quality product",
+    },
+
+    {
+      _id: "2",
+      name: "Nike Slim Shirt",
+      image: "/images/p2.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      reviews: 10,
+      description: "high quality product",
+    },
+    {
+      _id: "3",
+      name: "Nike Slim Shirt",
+      image: "/images/p3.jpg",
+      price: 40,
+      brand: "Nike",
+      rating: 4.5,
+      reviews: 10,
+      description: "high quality product",
+    },
+    {
+      _id: "4",
+      name: "Nike Slim Shirt",
+      image: "/images/p4.jpg",
+      price: 80,
+      brand: "Nike",
+      rating: 4.5,
+      reviews: 10,
+      description: "high quality product",
+    },
+    {
+      _id: "5",
+      name: "Nike Slim Shirt",
+      image: "/images/p5.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      reviews: 10,
+      description: "high quality product",
+    },
+  ],
+};
+
+export default data;
